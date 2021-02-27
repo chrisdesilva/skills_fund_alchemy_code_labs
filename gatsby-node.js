dc80e3796ad90a5d1af458e1e75d52a58d2c6760
Arthur Kang <akang@goalsolutions.com>
2021-02-27 00:02:54 -0800

    Fix Redirect

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,8 +2,8 @@ exports.createPages = ({ graphql, actions }) => {
     const { createPage, createRedirect } = actions
 
     createRedirect({
-        fromPath: "https://alchemycodelab.skills.fund/*",
-        toPath: "https://codefellowspdx.skills.fund/:splat",
+        fromPath: "https://codefellowspdx.skills.fund/*",
+        toPath: "https://alchemycodelab.skills.fund/:splat",
         isPermanent: true,
         force: true,
     })
